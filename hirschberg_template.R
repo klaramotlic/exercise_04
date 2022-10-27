28cc6d4a2000d9162492b159242ee296209916eb
Klara Motlickova <xmotli04@vutbr.cz>
2022-10-27 10:22:07 +0200

    working on hirschberg function, strugling with needleman wunsch function

--- hirschberg_template.R
+++ hirschberg_template.R
@@ -12,66 +12,94 @@ hirschberg_template <- function(seq1, seq2, align, match, mismatch, gap){
     second_align_row <- align[[2]] # initialize the second row of alignment
   
   
-    if # length of seq1 is equal to zero
+    if (length(seq1) == 0)# length of seq1 is equal to zero
     {
-        for # for each character in seq2
+        for (k in 1:length(seq2))# for each character in seq2
         {
-            first_align_row <- # add gap
-            second_align_row <- # add character from seq2
+            first_align_row <- append(first_align_row, '-')# add gap
+            second_align_row <- append(second_align_row, seq2[k])# add character from seq2
         }
         align <- c(DNAStringSet(first_align_row), DNAStringSet(second_align_row))
         print(align)
     }
-    else if # length of seq2 is equal to zero
+    else if (length(seq2) == 0)# length of seq2 is equal to zero
     {
-        for # for each character in seq1
+        for (l in 1:length(seq1))# for each character in seq1
         {
-            first_align_row <- # add character from seq1
-            second_align_row <- # add gap
+            first_align_row <- append(first_align_row, seq1[l])# add character from seq1
+            second_align_row <- append(second_align_row, '-')# add gap
         }
         align <- c(DNAStringSet(first_align_row), DNAStringSet(second_align_row))
         print(align)
     }
-    else if # length of seq1 and seq2 is equal to 1
+    else if (length(seq1) = 1 & length(seq2) = 1)# length of seq1 and seq2 is equal to 1
     {
-        first_align_row <- # add character from seq1
-        second_align_row <- # add character from seq2
+        first_align_row <- append(first_align_row, seq1)# add character from seq1
+        second_align_row <- append(second_align_row, seq2)# add character from seq2
         align <- c(DNAStringSet(first_align_row), DNAStringSet(second_align_row))
         print(align)
     }
     else
     {
-        x_len <- # length of seq1
-        x_mid <- # half of the length of seq1
-        y_len <- # length of seq2
+        x_len <- length(seq1)# length of seq1
+        x_mid <- floor(length(seq1)/2)# half of the length of seq1
+        y_len <- length(seq2)# length of seq2
         
-        left_score <- # NW score for the first half of seq1 and the whole seq2
-        right_score <- # NW score for the second half of seq1 and the whole seq2 (both are reversed)
-        y_mid <- # index of division for seq2
+        left_score <- nw(seq1[1:x_mid], seq2)# NW score for the first half of seq1 and the whole seq2
+        right_score <- nw(seq1[x_mid:end], seq2)# NW score for the second half of seq1 and the whole seq2 (both are reversed)
+        y_mid <- max(left_score + rev(right_score))# index of division for seq2
 
         # The first half
-        if # index of division for seq2 is equal to 0
+        if (y_mid = 0)# index of division for seq2 is equal to 0
         {
-            align <- # call hirschberg function for the first half of seq1 and for an empty DNAString object
+            align <- hirshberg_template(seq1[1:x_mid], ' ', align, match, mismatch, gap)# call hirschberg function for the first half of seq1 and for an empty DNAString object
         }
         else
         {
-            align <- # call hirschberg function for the first half of seq1 and for the first part of seq2
+            align <- hirshberg_template(seq1[1:x_mid], seq2[1:y_mid], align, match, mismatch, gap)# call hirschberg function for the first half of seq1 and for the first part of seq2
         }
         
         # The second half
         if ((x_mid + 1) > x_len) # seq1 cannot be further divided
         {
-            align <- # call hirschberg function for an empty DNAString object and the second half of seq2
+            align <- hirshberg_template(' ', seq2[y_mid:end], align, match, mismatch, gap)# call hirschberg function for an empty DNAString object and the second half of seq2
         }
         else if ((y_mid + 1) > y_len) # seq2 cannot be further divided
         {
-            align <- # call hirschberg function for the second half of seq1 and for an empty DNAString object
+            align <- hirshberg_template(seq1[x_mid:end], ' ', align, match, mismatch, gap)# call hirschberg function for the second half of seq1 and for an empty DNAString object
         }
         else 
         {
-            Align <- # call hirschberg function for the second half of seq1 and the second part of seq2
+            Align <- hirshberg_template(seq1[x_mid:end], seq2[y_mid:end], align, match, mismatch, gap)# call hirschberg function for the second half of seq1 and the second part of seq2
         }
     }
     return(align)
+}
+
+
+nw <- function(seq1, seq2){
+  Score(0, 0) <- 0 // 2 * (length(seq2) + 1)
+  for (j in 1:length(seq2)){
+    Score(0, j) = Score(0, j - 1) + (seq2[j])
+  }
+  for (i in 1:length(seq1)){
+    Score(1, 0) = Score(0, 0) + (seq1[i])
+    for (j in 1:length(seq2)){
+      scoreSub <- Score(0, j - 1) + (seq1[i], seq2[j])
+      scoreDel <- Score(0, j) + (seq1[i])
+      scoreIns <- Score(1, j - 1) + (seq2[j])
+      Score(1, j) = max(scoreSub, scoreDel, scoreIns)
+      end
+    }
+      
+    }
+  
+  // Copy Score[1] to Score[0]
+  Score(0, :) = Score(1, :)
+  end
+  for (j in 0:length(seq2)){
+    LastLine[j] <- Score(1, j)
+  }
+  
+  return LastLine
 }
